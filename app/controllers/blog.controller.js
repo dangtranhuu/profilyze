0c6efcaf8b897e8e8c440ed8a5cce0e10b655675
theanishtar <dangtt135@gmail.com>
2024-04-10 11:59:36 +0700

    feat: change icon and style view url

--- app/controllers/blog.controller.js
+++ app/controllers/blog.controller.js
@@ -49,58 +49,46 @@ exports.view = async (req, res) => {
 
     console.log(result[0].view_count);
     svgString = `
-      <svg xmlns="http://www.w3.org/2000/svg" width="500" height="150">
-      <style>
-        .code {
-          --code-ln-color: #9e9e9e;
-          --inline-code-color: #c52950;
-          --inline-code-bg-color: #f9f2f4;
-          --font-family-code: Consolas, Monaco, "Andale Mono", "Ubuntu Mono", monospace;
-          -webkit-font-smoothing: antialiased;
-          display: inline;
-          line-height: 23.12px;
-          font-size: 0.875em;
-          background: transparent;
-          font-weight: 600;
-          font-family: var(--font-family-code);
-          color: var(--inline-code-color);
-          padding: 0.25rem 0.5rem;
-          margin: 0;
-          background-color: var(--inline-code-bg-color);
-          border-radius: 3px;
-          overflow-wrap: break-word;
-          transition: background-color var(--t-color);
-        }
-    
-        .dark .code {
-          --code-ln-color: #9e9e9e;
-          --inline-code-color: #c52950;
-          --inline-code-bg-color: #f9f2f4;
-          --font-family-code: Consolas, Monaco, "Andale Mono", "Ubuntu Mono", monospace;
-          color-scheme: dark;
-          -webkit-font-smoothing: antialiased;
-          display: inline;
-          line-height: 23.12px;
-          font-size: 0.875em;
-          background: transparent;
-          font-weight: 600;
-          font-family: var(--font-family-code);
-          color: var(--inline-code-color);
-          padding: 0.25rem 0.5rem;
-          margin: 0;
-          background-color: var(--inline-code-bg-color);
-          border-radius: 3px;
-          overflow-wrap: break-word;
-          transition: background-color var(--t-color);
-        }
-      </style>
-      <foreignObject width="500" height="150">
-        <body xmlns="http://www.w3.org/1999/xhtml">
-        <image xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTYiIGhlaWdodD0iMTYiIHZpZXdCb3g9IjAgMCAxNiAxNiIgZmlsbD0ibm9uZSI+CiAgICA8cGF0aCBkPSJNOC4zNSAxMC4zNWwtMy41LTMuNTRoMy41M3oiLz4KPC9zdmc+" width="16" height="16"/>
-        <code class="code">👀 ${result[0].view_count} lượt xem</code>
-        </body>
-      </foreignObject>
-    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="106" height="20" role="img"
+    aria-label="👀">
+    <title>👀</title>
+    <style>
+      a:hover #llink {
+        fill: url(#b);
+        stroke: #ccc
+      }
+
+      a:hover #rlink {
+        fill: #4183c4
+      }
+    </style>
+    <linearGradient id="a" x2="0" y2="100%">
+      <stop offset="0" stop-color="#fcfcfc" stop-opacity="0" />
+      <stop offset="1" stop-opacity=".1" />
+    </linearGradient>
+    <linearGradient id="b" x2="0" y2="100%">
+      <stop offset="0" stop-color="#ccc" stop-opacity=".1" />
+      <stop offset="1" stop-opacity=".1" />
+    </linearGradient>
+    <g stroke="#d5d5d5">
+      <rect stroke="none" fill="#fcfcfc" x="0.5" y="0.5" width="78" height="19" rx="2" />
+      <rect x="84.5" y="0.5" width="21" height="19" rx="2" fill="#fafafa" />
+      <rect x="84" y="7.5" width="0.5" height="5" stroke="#fafafa" />
+      <!-- Thay thế đoạn mã icon GitHub bằng icon Streak -->
+      <path d="M84.5 6.5 l-3 3v1 l3 3" stroke="d5d5d5" fill="#fafafa" />
+    </g>
+    <!-- Thay thế dữ liệu base64 của icon GitHub bằng dữ liệu base64 của icon Streak -->
+    <g aria-hidden="true" fill="#333" text-anchor="middle" font-family="Helvetica Neue,Helvetica,Arial,sans-serif"
+      text-rendering="geometricPrecision" font-weight="700" font-size="110px" line-height="14px">
+      <!-- Thay đổi giá trị của x và y để đặt icon vào vị trí mong muốn -->
+      <text x="10" y="15"  font-size="15px" line-height="14px">👀</text>
+      <rect id="llink" stroke="#d5d5d5" fill="url(#a)" x=".5" y=".5" width="78" height="19" rx="2" /><text
+        aria-hidden="true" x="475" y="150" fill="#fff" transform="scale(.1)" textLength="510">Lượt xem</text><text
+        x="475" y="140" transform="scale(.1)" textLength="510">Lượt xem</text><text aria-hidden="true" x="945" y="150"
+        fill="#fff" transform="scale(.1)" textLength="130">${result[0].view_count}</text><text id="rlink" x="945" y="140"
+        transform="scale(.1)" textLength="130">${result[0].view_count}</text>
+    </g>
+  </svg>
     `;
 
     res.set('Content-Type', 'image/svg+xml');
